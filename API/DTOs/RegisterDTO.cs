5bd072f7d6165b2892ca725ef5612ff4fd4e6b18
cherryApp <j.cserko@gmail.com>
2024-06-11 11:51:21 +0200

    feat: basic validation

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -1,7 +1,12 @@
-﻿namespace API.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
 
 public class RegisterDTO
 {
+    [Required]
     public string Username { get; set; }
+    
+    [Required]
     public string Password { get; set; }
 }
