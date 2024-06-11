9ad1b8708fc00b7ee6a86c5569fbeb0af2331694
cherryApp <j.cserko@gmail.com>
2024-06-11 11:33:24 +0200

    feat: add DTO

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class AccountController : BaseApiController
+{
+    private readonly DataContext _context;
+
+    public AccountController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")] // /api/account/register
+    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+    {
+
+        if (await UserExists(registerDTO.Username)) 
+            return BadRequest("Username is taken");
+
+        using var hash = new HMACSHA512();
+        var user = new AppUser
+        {
+            UserName = registerDTO.Username.ToLower(),
+            PasswordHash = hash.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+            PasswordSalt = hash.Key
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+    }
+
+}
